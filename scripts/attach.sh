8c8ea39b815ef795e3cf2a795b2b2af1813208f1
blockcheq-dev <development@blockcheq.com>
2018-07-29 19:53:07 +0200

    Updated attach.sh

--- scripts/attach.sh
+++ scripts/attach.sh
@@ -2,7 +2,8 @@
 set -u
 set -e
 
-geth attach ipc:/home/blockcheq/blockcheq/data/geth.ipc
+# geth attach ipc:/home/blockcheq/blockcheq/data/geth.ipc
+geth attach ipc:/root/blockcheq/data/geth.ipc
 
 set +u
 set +e
